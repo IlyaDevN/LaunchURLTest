e4f4ddefac29e8a5d3ad665fdb43c78084927dc1
inieviedrov <inieviedrov@spribe.co>
2025-12-05 00:29:14 +0200

    feat: Add webmanifest and cache bust

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,25 +3,27 @@
 import clsx from "clsx";
 import "../styles/global.css";
 import { Inter } from "next/font/google";
-// 1. Импортируем компонент Head для управления мета-тегами
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  // Получаем базовый путь (на локалке пустая строка, на проде "/LaunchURLTest")
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
   return (
     <div className={clsx(inter.className, "text-slate-900")}>
-      {/* 2. Добавляем блок Head с настройками заголовка и иконки */}
       <Head>
-          <title>Toolbox Launch URL</title>
-          <meta name="description" content="iGaming URL Validator and Tools" />
+        <title>Toolbox Launch URL</title>
+        <meta name="description" content="iGaming URL Validator and Tools" />
+        
+        {/* Подключаем Manifest */}
+        <link rel="manifest" href={`${basePath}/site.webmanifest`} />
 
-          {/* Основная иконка */}
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
-          {/* Apple Touch Icon (для закладок на айфонах/маках - будет использовать тот же svg, 
-              но лучше в будущем сделать png, пока сойдет как заглушка) */}
-          <link rel="apple-touch-icon" href="/favicon.svg" />
+        {/* Подключаем иконки. Добавил ?v=2, чтобы сбросить кеш браузера */}
+        <link rel="icon" type="image/svg+xml" href={`${basePath}/favicon.svg?v=2`} />
+        <link rel="alternate icon" href={`${basePath}/favicon.svg?v=2`} />
+        <link rel="apple-touch-icon" href={`${basePath}/favicon.svg?v=2`} />
       </Head>
 
       <Component {...pageProps} />
